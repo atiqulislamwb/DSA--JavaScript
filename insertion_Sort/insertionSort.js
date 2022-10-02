d4e5525a921c8b4692ae69aee0553b9fda726a31
atiqulislamwb <atiqulislamrussell0@gmail.com>
2022-10-02 20:05:26 +0600

    insertion and selection sort

---
+++ insertion_Sort/insertionSort.js
@@ -0,0 +1,15 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentElement = arr[i];
+    for (let j = i - 1; j >= 0; j--) {
+      if (arr[j] > currentElement) {
+        arr[j + i] = arr[j];
+      }
+    }
+    arr[j + 1] = currentElement;
+    console.log(arr);
+  }
+  return arr;
+}
+
+console.log(insertionSort([12, 2, 3, 1, 77, 65, 89, 900, 55]));
---
+++ selection_Sort/selectionSort.js
@@ -0,0 +1,39 @@
+//selection sort --> instead of first placing large values into sorted position ,
+// it places small values into sorted position ...
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) {
+      let temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
+  }
+  return arr;
+}
+console.log(selectionSort([32, 44, 11, 56, 99, 76, 43, 23]));
+
+//es6 version of selection
+function selectionSort2(arr) {
+  const swap = (arr, idx1, idx2) =>
+    ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
+
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[lowest] > arr[j]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) swap(arr, i, lowest);
+  }
+  return arr;
+}
+
+console.log(selectionSort2([32, 44, 11, 56, 99, 76, 43, 23]));
