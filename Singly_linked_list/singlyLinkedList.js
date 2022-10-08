52e133e6e91d5a4fe7a838e0702d83a04f55c98b
atiqulislamwb <atiqulislamrussell0@gmail.com>
2022-10-08 20:53:28 +0600

    singly linked list done

--- Singly_linked_list/singlyLinkedList.js
+++ Singly_linked_list/singlyLinkedList.js
@@ -1,5 +1,21 @@
 //ordered lists of data contains head tail  and length property
 //linked list consist of nodes, and each node has a value and a pointer to another node or value
+//push---> add something to the array at the end position
+//pop---> remove something to the array at the end position
+//shift---> remove something to the array at the first position
+//unshift---> add something to the array at the first position
+//get ---> retrieving a node by its position in the linked list
+//set-->changing the value of the node based on its position in the linked list
+//insert--> adding a node from the linked list at a specific position
+//remove--> remove a node from the linked list at a specific position
+//reverse--> reversing the linked list in place
+
+//Big O --->
+
+// Insertion O(1)
+// Removal O(1) or O(n)
+// Searching O(n)
+// Access O(n)
 
 class Node {
   constructor(val) {
@@ -26,11 +42,140 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  traverse() {
+    let current = this.head;
+    while (current) {
+      console.log(current.val);
+      current = current.next;
+    }
+  }
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+    return current;
+  }
+  set(index, val) {
+    let foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+  insert(index, val) {
+    if (index < 0 || index >= this.length) return null;
+    if (index === this.length) {
+      this.push(val);
+      return true;
+    }
+    if (index === 0) return !!this.unshift(val);
+    let newNode = new Node(val);
+    let prev = this.get(index - 1);
+    let temp = prev.next;
+    prev.next = newNode;
+    newNode = temp;
+    this.length++;
+    return true;
+  }
+  remove(index) {
+    if (index < 0 || index >= this.length) return null;
+    if (index === this.length) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    let prevNode = this.get(index - 1);
+    let removed = prevNode.next;
+    prevNode.next = removed.next;
+    this.length--;
+
+    return removed;
+  }
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let prev = null;
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
+
+  print() {
+    let arr = [];
+    let current = this.head;
+    while (current) {
+      arr.push(current.val);
+      current = current.next;
+    }
+    console.log(arr);
+  }
 }
 
 let list = new SinglyLinkedList();
 list.push(2);
 list.push(3);
 list.push(5);
-
-console.log(list);
+list.push(10);
+list.push(44);
+list.push(77);
+list.pop();
+list.shift();
+list.unshift(12);
+console.log(list.get(2));
+console.log(list.set(1, 3));
+console.log(list.insert(1, 99));
+list.print();
+list.reverse();
+list.print();
