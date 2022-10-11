1e836fc5463960cfda6b24d602178900c7a35190
atiqulislamwb <atiqulislamrussell0@gmail.com>
2022-10-11 15:58:01 +0600

    binary search tree

---
+++ Binary_Search_Tree/binarySearch.js
@@ -0,0 +1,74 @@
+//Binary search tree (BST) has tow children from the root node ................
+//everything is left less than root and everything in right greater than root
+//Big O -->
+// 1.Insertion O(log n)
+// 2.Searching O(log n)
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    let newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+
+  find(value) {
+    if (this.root === null) return false;
+    let current = this.root;
+    let found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+
+    return current;
+  }
+}
+
+let tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
+tree.find(7);
+console.log(tree);
