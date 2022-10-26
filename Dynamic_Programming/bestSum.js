804b6a8a057c10cffbff58c1fec208c30d4bbfff
atiqulislamwb <atiqulislamrussell0@gmail.com>
2022-10-26 15:19:13 +0600

    bestSum

---
+++ Dynamic_Programming/bestSum.js
@@ -0,0 +1,26 @@
+const bestSum = (targetSum, numbers, memo = {}) => {
+  if (targetSum in memo) return memo[targetSum];
+  if (targetSum === 0) return [];
+  if (targetSum < 0) return null;
+  let shortestCombination = null;
+  for (let num of numbers) {
+    const remainder = targetSum - num;
+    const result = bestSum(remainder, numbers, memo);
+    if (result !== null) {
+      const combination = [...result, num];
+      if (
+        shortestCombination === null ||
+        combination.length < shortestCombination.length
+      ) {
+        shortestCombination = combination;
+      }
+    }
+  }
+
+  memo[targetSum] = shortestCombination;
+  return shortestCombination;
+};
+
+console.log(bestSum(7, [5, 3, 4, 7]));
+console.log(bestSum(8, [2, 3, 5]));
+console.log(bestSum(100, [1, 2, 5, 25])); //[25,25,25,25]
